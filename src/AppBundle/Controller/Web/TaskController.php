46cd8b955bd21f23039ff0f04d37224ee2137087
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2016-01-05 13:11:19 +0100

    Adding task

--- src/AppBundle/Controller/Web/TaskController.php
+++ src/AppBundle/Controller/Web/TaskController.php
@@ -2,19 +2,40 @@
 
 namespace AppBundle\Controller\Web;
 
+use AppBundle\Form\Type\TaskType;
+use Domain\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends Controller
 {
-    public function addTaskAction()
+    public function addTaskAction(Request $request)
     {
-        return $this->render(':Tasks:add.html.twig');
+        $form = $this->createForm(TaskType::class);
+
+        $form->handleRequest($request);
+
+        $response = $this->render(':Tasks:add.html.twig', ['form' => $form->createView()]);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+            $task = new Task($data['name']);
+            $this->get('repository.task')->add($task);
+
+            $request->getSession()->getFlashbag()->add(
+                'success',
+                sprintf('Task "%s" successfully created', $task->getName())
+            );
+            $response = $this->redirectToRoute('web_tasks_list');
+        }
+
+        return $response;
     }
 
     public function getTasksAction()
     {
         $tasks = $this->get('repository.task')->findAll();
 
-        return $this->render(':Tasks:list.html.twig', array('tasks' => $tasks));
+        return $this->render(':Tasks:list.html.twig', ['tasks' => $tasks]);
     }
 }
