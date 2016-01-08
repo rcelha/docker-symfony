52f833c9f3116f826bfbc311d7b0db3665c53e8d
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2016-01-08 17:10:47 +0100

    Changed CircleCI configuration

--- startup.sh
+++ startup.sh
@@ -3,3 +3,7 @@
 cd /var/www/docker-symfony
 
 composer install -n --optimize-autoloader
+
+php app/console cache:clear --env=prod --no-debug
+
+chown -R www-data:www-data /tmp/sf2 && chmod -R 770 /tmp/sf2
